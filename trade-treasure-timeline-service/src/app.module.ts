1b0fb70c55975bf075824dad93a2983904924d42
florinpana01 <florinpana98@gmail.com>
2023-11-11 12:48:12 +0100

    microservice architecture created + CRUD

--- trade-treasure-timeline-service/src/app.module.ts
+++ trade-treasure-timeline-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
