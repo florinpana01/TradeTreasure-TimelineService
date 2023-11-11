416dff5ea04285e632358631f801285efabd0b25
florinpana01 <florinpana98@gmail.com>
2023-11-12 00:03:34 +0100

    unit tests + first pipeline file version

--- trade-treasure-timeline-service/src/like/like.service.spec.ts
+++ trade-treasure-timeline-service/src/like/like.service.spec.ts
@@ -1,18 +1,43 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { LikeService } from './like.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Like } from "./like.entity";
+import { LikeService } from "./like.service";
 
 describe('LikeService', () => {
-  let service: LikeService;
+    let service: LikeService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [LikeService],
-    }).compile();
+    const mockLikeRepository = {
+        save: jest.fn().mockImplementation((like: Like) => Promise.resolve(like)),
+        update: jest.fn().mockImplementation((id, data) => ({id, ...data})),
+        find: jest.fn().mockImplementation(() => Promise.resolve([])),
+        findOne: jest.fn().mockImplementation((id) => Promise.resolve({
+            productId: 1,
+            userId: 1,
+        }))
+    }
 
-    service = module.get<LikeService>(LikeService);
-  });
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [LikeService, {
+                provide: getRepositoryToken(Like),
+                useValue: mockLikeRepository
+            }],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+        service = module.get<LikeService>(LikeService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('should create a new like', async () => {
+        const data = {
+            productId: 1,
+            userId: 1,
+        }
+
+        const savedLike = await service.create(data);
+        expect(savedLike).toBeDefined();
+    });
+})
