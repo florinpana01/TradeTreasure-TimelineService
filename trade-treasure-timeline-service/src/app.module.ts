3e50775a87935ad712a039f15a4b809d6e9c682f
florinpana01 <florinpana98@gmail.com>
2023-11-11 18:46:58 +0100

    like functionality added

--- trade-treasure-timeline-service/src/app.module.ts
+++ trade-treasure-timeline-service/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductController } from './product/product.controller';
 import { ProductModule } from './product/product.module';
+import { LikeController } from './like/like.controller';
+import { LikeModule } from './like/like.module';
 
 @Module({
-  imports: [ProductModule],
-  controllers: [AppController],
+  imports: [ProductModule, LikeModule],
+  controllers: [AppController, LikeController],
   providers: [AppService],
 })
 export class AppModule {}
